e213a17b6fd4beeeba77dbc5f2349a9964b8f1c8
Charlon Palacay <charlon@gmail.com>
2014-12-17 15:32:14 -0800

    Seperates badge loading into two functions for scrolling and below fold loading.

--- pjaxified/static/trestle/js/example.js
+++ pjaxified/static/trestle/js/example.js
@@ -2,6 +2,7 @@ var processing_badges = false;
 var pathname;
 var data;
 var first_page = true;
+var last_page = false;
 var next_url;
 var last_index;
     
@@ -61,11 +62,11 @@ $(window).scroll(function() {
         // if scrolled to the bottom... AND currently not processing any badge requests (bounce hack)
         if($(window).scrollTop() + $(window).height() == $(document).height() && !processing_badges) {
             
-            console.log("you scrolled to the bottom");    
+            //console.log("you scrolled to the bottom");    
             
             $("#badge_list_loading").show();
             
-            loadBadgeList();                 
+            loadScrollingBadgeList();                 
         }
         
     }
@@ -80,56 +81,43 @@ function handleRoutes() {
     // "badges" page was loaded
     if(pathname.indexOf("/badges/") >= 0) {
               
-       loadBadgeList(); 
+       loadRemainingBadgeList(); 
     }
 }
 
 // HANDLEBARS FUNCTIONS
 
-function loadBadgeList() {
-    
-    var protocol = window.location.protocol;
-    var host = window.location.host;
+
+function loadRemainingBadgeList() {
     
     last_index = $('#badge_list').attr("data-last-index");
     
-    // start on page 1 if prev is null
-    //url = protocol + '//' + host + '/api/v1/badges?page=1&format=json'
+    url = 'http://curry.eplt.washington.edu:8000/api/v1/badges?page=1&format=json';
     
-    if (first_page) {
-        url = 'http://curry.eplt.washington.edu:8000/api/v1/badges?page=1&format=json';
-    }
-    else {
-        url = next_url;    
-    }
-    
-    // http://curry.eplt.washington.edu:8000/api/v1/badges?page=2&format=json
-                
-    // make an ajax request for the badgelist partial    
     $.ajax({
         type: 'GET',
         url: url,
         beforeSend:function(){
-                        
+                                    
             // show the badge loading spinner
             $("#badge_list_loading").show();
-            
+             
         },    
         success:function(data){
             
+            $("#badge_list_loading").show();
+            
             // exclude data that was originally printed -- only if on page1 (prev null)
-            if (data.previous == null) {
-                data.results.splice(0, last_index); 
-                first_page = false;
-            }   
-            
+            data.results.splice(0, last_index); 
+     
             next_url = data.next;
-            
             console.log(next_url);
+            
+            url = next_url;
                                   
             var context = { badges: data };
             
-            console.log(context);
+            //console.log(context);
                             
             // compile handlebars template and render
             var template = Handlebars.compile($('#tpl-badge-list').html()),
@@ -137,9 +125,82 @@ function loadBadgeList() {
                                             
             // paint it in the badge list container
             $("#badge_list").append(rendered);
-                          
+                              
+            
             // hide the badge loading spinner
             $("#badge_list_loading").hide();
+      
+            
+        },
+        error:function() {
+            console.log("error fetching json...");
+        }
+    });
+
+
+}
+
+function loadScrollingBadgeList() {
+    
+    var protocol = window.location.protocol;
+    var host = window.location.host;
+    
+    
+    
+    // start on page 1 if prev is null
+    //url = protocol + '//' + host + '/api/v1/badges?page=1&format=json'
+    
+    
+                        
+    // make an ajax request for the badgelist partial    
+    $.ajax({
+        type: 'GET',
+        url: url,
+        beforeSend:function(){
+                                    
+            // show the badge loading spinner
+            $("#badge_list_loading").show();
+             
+        },    
+        success:function(data){
+            
+            $("#badge_list_loading").show();
+            
+            // exclude data that was originally printed -- only if on page1 (prev null)
+            if (first_page) {
+                data.results.splice(0, last_index); 
+                first_page = false;
+            }   
+            
+            if (!last_page) {
+                
+                next_url = data.next;
+                console.log(next_url);
+                
+                url = next_url;
+                                      
+                var context = { badges: data };
+                
+                //console.log(context);
+                                
+                // compile handlebars template and render
+                var template = Handlebars.compile($('#tpl-badge-list').html()),
+                    rendered = template(context);
+                                                
+                // paint it in the badge list container
+                $("#badge_list").append(rendered);
+                              
+            }
+            
+            if (next_url == null) {
+                last_page = true;       
+            }   
+  
+            
+            // hide the badge loading spinner
+            $("#badge_list_loading").hide();
+      
+            
             
         },
         error:function() {
